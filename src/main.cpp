3b4a4cb142a0315d5acb69b7601409ddd9d89e6f
Ryan Gibson <gibsorya@gmail.com>
2025-09-04 17:35:56 -0500

    Init SDL window

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include "version.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cout << argv[0] << " Version " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR
+                  << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    }
+
+    bool done = false;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window =
+        SDL_CreateWindow("OpenGL Window", 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    if (!window) {
+        std::cerr << "CreateWindow Error" << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+
+    while (!done) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
+}
