e0d75023a61313737c59e9490a71d2d84c3f5366
Ryan Gibson <gibsorya@gmail.com>
2025-09-05 16:50:05 -0500

    add packages for project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "version.h"
+
+#include <glad/gl.h>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
+#include <glm/glm.hpp>
+
 #include <iostream>
 
 int main(int argc, char **argv) {
